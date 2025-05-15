1cf92bac2adbf973bb0ec716434d87b1fc771ae3
김선민 <tjsals116@gmail.com>
2025-05-15 20:00:28 +0900

    ✨ update Vite config to use ENV for allowedHosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,45 +5,46 @@ import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { copyFileSync } from 'node:fs';
 import dotenv from 'dotenv';
+import { ENV } from '../../packages/env';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-export default defineConfig(({ }) => {
+export default defineConfig(() => {
   return {
-  server: {
+    server: {
 
-    allowedHosts: ['boroployment.loca.lt'],
-  },
-  plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    tailwindcss(),
-    react(),
-    {
-      name: 'generate-404-html', // 플러그인 이름
-      apply: 'build', // build 단계에서 실행
-      closeBundle() {
-        try {
-          copyFileSync('dist/index.html', 'dist/404.html');
-          console.log('✅ 404.html created successfully');
-        } catch (error) {
-          console.error('❌ Failed to create 404.html:', error);
-        }
-      },
+      allowedHosts: [ENV.CLIENT_URI],
     },
-  ],
-  base: '/',
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@repo', replacement: path.resolve(__dirname, '../../packages') }
+    plugins: [
+      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+      tailwindcss(),
+      react(),
+      {
+        name: 'generate-404-html', // 플러그인 이름
+        apply: 'build', // build 단계에서 실행
+        closeBundle() {
+          try {
+            copyFileSync('dist/index.html', 'dist/404.html');
+            console.log('✅ 404.html created successfully');
+          } catch (error) {
+            console.error('❌ Failed to create 404.html:', error);
+          }
+        },
+      },
     ],
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  define: {
-    'process.env': process.env,
-  },
-}
+    base: '/',
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
+        { find: '@repo', replacement: path.resolve(__dirname, '../../packages') }
+      ],
+    },
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+    define: {
+      'process.env': process.env,
+    },
+  }
 });
