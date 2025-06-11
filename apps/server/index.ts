e33a6f863a606ead490ad5e3bf57c3eb972f9f76
김선민 <tjsals116@gmail.com>
2025-06-11 10:23:47 +0900

    ✨ Update server port configuration and enhance package dependencies

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -8,10 +8,17 @@ import MercuriusLogging from 'mercurius-logging';
 import { ENV } from '@repo/env'
 import path from 'node:path';
 import dotenv from 'dotenv';
+import { match, P } from 'ts-pattern';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const serverUri = new URL(ENV.SERVER_URL);
+
+const port = match(serverUri)
+  .with({ port: P.string.minLength(1) }, (uri) => Number(uri.port))
+  .with({ protocol: 'http:' }, () => 3000)
+  .with({ protocol: 'https:' }, () => 443)
+  .otherwise(() => 8000);
 
 const FastifyApp = Fastify({
   logger: true,
@@ -46,8 +53,8 @@ FastifyApp.get('/health', async function () {
   return { status: 'ok' }
 })
 
-FastifyApp.listen({ port: Number(serverUri.port) }, (_, args2) => {
-  console.log('Starting Server on ', serverUri.origin);
+FastifyApp.listen({ port: 8000 }, (_, args2) => {
+  console.log('Starting Server on ', serverUri.origin, `:${port}`);
 })
 
 
